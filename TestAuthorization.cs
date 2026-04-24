676854c7fe954d88dcb1bbcbf1041289555cd0f2
daniel <danieapplied@gmail.com>
2026-04-24 11:52:57 +0200

    Update TestAuthorization.cs

--- TestAuthorization.cs
+++ TestAuthorization.cs
@@ -1,6 +1,27 @@
-namespace SecurityAndAuth;
+using NUnit.Framework;
+using System;
 
+[TestFixture]
 public class TestAuthorization
 {
-    
+    [Test]
+    public void AdminAccess_ShouldPass()
+    {
+        var result = AuthorizationService.HasAccess("ADMIN", "ADMIN");
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void UserAccess_AdminRoute_ShouldFail()
+    {
+        Assert.Throws<UnauthorizedAccessException>(() =>
+            AuthorizationService.Authorize("USER", "ADMIN"));
+    }
+
+    [Test]
+    public void CaseInsensitiveRoles_ShouldPass()
+    {
+        var result = AuthorizationService.HasAccess("admin", "ADMIN");
+        Assert.IsTrue(result);
+    }
 }
